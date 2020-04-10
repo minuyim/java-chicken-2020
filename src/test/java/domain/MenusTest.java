5ecfc67084de98716e03a0200acac222f1ba5137
minuyim <minu_yim@naver.com>
2020-04-10 14:25:02 +0900

    feat : 주문 내역을 담당하는 Menus 기능 추가

---
+++ src/test/java/domain/MenusTest.java
@@ -0,0 +1,32 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MenusTest {
+	private Menus menus = new Menus();
+
+	@Test
+	@DisplayName("메뉴를 수량만큼 더한다.")
+	void addMenu() {
+		menus.addMenu(MenuRepository.findByNumber(1), 1);
+		assertThat(menus.getMenus().getOrDefault(MenuRepository.findByNumber(1), null)).isEqualTo(new Amount(1));
+	}
+
+	@Test
+	@DisplayName("주문 내역이 비었는지 확인")
+	void isEmpty() {
+		assertThat(menus.isEmpty()).isTrue();
+	}
+
+	@Test
+	@DisplayName("메뉴판 비우기")
+	void clear() {
+		menus.addMenu(MenuRepository.findByNumber(1), 1);
+		menus.clear();
+		assertThat(menus.isEmpty()).isTrue();
+	}
+}
