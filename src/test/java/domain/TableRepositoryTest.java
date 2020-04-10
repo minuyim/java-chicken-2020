8c05d89b59fd192260a1b33e0d19c1b968423f04
minuyim <minu_yim@naver.com>
2020-04-10 15:20:22 +0900

    style: 코드 포맷팅 수행

--- src/test/java/domain/TableRepositoryTest.java
+++ src/test/java/domain/TableRepositoryTest.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class TableRepositoryTest {
-	@Test
-	@DisplayName("Number로 테이블을 찾는 기능 테스트")
-	void findById() {
-		assertThat(TableRepository.findByNumber(1)).isEqualTo(TableRepository.tables().get(0));
-	}
+    @Test
+    @DisplayName("Number로 테이블을 찾는 기능 테스트")
+    void findById() {
+        assertThat(TableRepository.findByNumber(1)).isEqualTo(TableRepository.tables().get(0));
+    }
 
-	@Test
-	@DisplayName("Number로 메뉴를 찾는 기능 테스트 - 예외처리")
-	void findByIdWithException() {
-		assertThatThrownBy(() -> TableRepository.findByNumber(0))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessage("해당 번호를 가진 테이블이 없습니다.");
-	}
+    @Test
+    @DisplayName("Number로 메뉴를 찾는 기능 테스트 - 예외처리")
+    void findByIdWithException() {
+        assertThatThrownBy(() -> TableRepository.findByNumber(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("해당 번호를 가진 테이블이 없습니다.");
+    }
 }
