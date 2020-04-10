f04d8c45b469dc4fa32e5dc4f1c1df624c27e5b8
minuyim <minu_yim@naver.com>
2020-04-10 15:17:24 +0900

    feat: 주문 내역 가격 계산을 위한 Amount 계산 기능 추가
    - Amount 2개의 객체를 더하는 기능
    - multiply, divide 기능 추가
    - 0 이하일 경우 예외 처리

--- src/main/java/domain/Amount.java
+++ src/main/java/domain/Amount.java
@@ -3,36 +3,54 @@ package domain;
 import java.util.Objects;
 
 public class Amount {
-	private static final int MAX_AMOUNT = 99;
-	private int amount;
+    private static final int MAX_AMOUNT = 99;
+    private int amount;
 
-	public Amount(int amount) {
-		validate(amount);
-		this.amount = amount;
-	}
+    public Amount(int amount) {
+        validate(amount);
+        this.amount = amount;
+    }
 
-	private void validate(int amount) {
-		if (amount > MAX_AMOUNT) {
-			throw new IllegalArgumentException("최대 수량을 넘겼습니다.");
-		}
-	}
+    private void validate(int amount) {
+        if (amount > MAX_AMOUNT) {
+            throw new IllegalArgumentException("최대 수량을 넘겼습니다.");
+        }
+        if (amount < 0) {
+            throw new IllegalArgumentException("수량은 음수가 될 수 없습니다.");
+        }
+    }
 
-	public Amount add(int amount) {
-		return new Amount(this.amount + amount);
-	}
+    public static Amount sum(Amount amount1, Amount amount2) {
+        return new Amount(amount1.amount + amount2.amount);
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Amount amount1 = (Amount) o;
-		return amount == amount1.amount;
-	}
+    public Amount add(int amount) {
+        return new Amount(this.amount + amount);
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(amount);
-	}
+    public int multiply(int price) {
+        return price * amount;
+    }
+
+    public int divide(int number) {
+        if (number <= 0) {
+            throw new IllegalArgumentException("0 이상의 값을 입력해주세요");
+        }
+        return amount / number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Amount amount1 = (Amount) o;
+        return amount == amount1.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
--- src/test/java/domain/AmountTest.java
+++ src/test/java/domain/AmountTest.java
@@ -5,26 +5,49 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class AmountTest {
-	@Test
-	@DisplayName("Amount 생성 테스트")
-	void construct() {
-		assertThat(new Amount(10)).isNotNull();
-	}
+    @Test
+    @DisplayName("Amount 생성 테스트")
+    void construct() {
+        assertThat(new Amount(10)).isNotNull();
+    }
 
-	@Test
-	@DisplayName("Amount 최대 초과 시 예외처리")
-	void constructWithException() {
-		assertThatThrownBy(() -> new Amount(100))
-				.isInstanceOf(IllegalArgumentException.class)
-				.hasMessage("최대 수량을 넘겼습니다.");
-	}
+    @Test
+    @DisplayName("Amount 최대 초과 시 예외처리")
+    void constructWithExceptionNegative() {
+        assertThatThrownBy(() -> new Amount(-1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수량은 음수가 될 수 없습니다.");
+    }
 
-	@Test
-	void add() {
-		Amount amount = new Amount(10);
-		assertThat(amount.add(10)).isEqualTo(new Amount(20));
-	}
+    @Test
+    @DisplayName("Amount 최대 초과 시 예외처리")
+    void constructWithException() {
+        assertThatThrownBy(() -> new Amount(100))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("최대 수량을 넘겼습니다.");
+    }
+
+    @Test
+    @DisplayName("int 더하기 테스트")
+    void add() {
+        Amount amount = new Amount(10);
+        assertThat(amount.add(10)).isEqualTo(new Amount(20));
+    }
+
+    @Test
+    @DisplayName("Amount 덧셈 테스트")
+    void sum() {
+        Amount amount1 = new Amount(10);
+        Amount amount2 = new Amount(20);
+        assertThat(Amount.sum(amount1, amount2)).isEqualTo(new Amount(30));
+    }
+
+    @Test
+    @DisplayName("곱셈 테스트")
+    void multiply() {
+        Amount amount = new Amount(10);
+        assertThat(amount.multiply(1000)).isEqualTo(10000);
+    }
 }
