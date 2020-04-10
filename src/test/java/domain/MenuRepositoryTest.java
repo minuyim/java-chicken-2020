7fb2bad1e7874b96a282f7a3becb3d9f7aa7891d
minuyim <minu_yim@naver.com>
2020-04-10 13:45:44 +0900

    feat : MenuRepository에서 번호에 해당하는 메뉴를 찾는 메소드 추가

---
+++ src/test/java/domain/MenuRepositoryTest.java
@@ -0,0 +1,23 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class MenuRepositoryTest {
+	@Test
+	@DisplayName("Number로 메뉴를 찾는 기능 테스트")
+	void findById() {
+		assertThat(MenuRepository.findByNumber(1)).isEqualTo(MenuRepository.menus().get(0));
+	}
+
+	@Test
+	@DisplayName("Number로 메뉴를 찾는 기능 테스트 - 예외처리")
+	void findByIdWithException() {
+		assertThatThrownBy(() -> MenuRepository.findByNumber(0))
+				.isInstanceOf(IllegalArgumentException.class)
+				.hasMessage("해당 번호를 가진 메뉴가 없습니다.");
+	}
+}
