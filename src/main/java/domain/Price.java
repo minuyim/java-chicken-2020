b90e094e10636bc0513c1aa74d5eb4b2a48b3dd8
minuyim <minu_yim@naver.com>
2020-04-10 15:50:18 +0900

    feat : 최종 가격을 담당하는 클래스 추가
    - 결제 방식에 따른 최종 가격을 계산하는 기능 담당

---
+++ src/main/java/domain/Price.java
@@ -0,0 +1,37 @@
+package domain;
+
+import java.util.Objects;
+
+public class Price {
+    private int price;
+
+    public Price(int price) {
+        validateNegative(price);
+        this.price = price;
+    }
+
+    private void validateNegative(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("가격은 음수가 될 수 없습니다.");
+        }
+    }
+
+    public int discount(Payment payment) {
+        return (int) payment.calculateDiscount(price);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Price price1 = (Price) o;
+        return price == price1.price;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(price);
+    }
+}
---
+++ src/test/java/domain/PriceTest.java
@@ -0,0 +1,24 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PriceTest {
+    @Test
+    @DisplayName("음수일 경우 예외처리")
+    void constructWithException() {
+        assertThatThrownBy(() -> new Price(-10))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("가격은 음수가 될 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("결제 방식에 따른 최종 결제 가격을 계산할 수 있는 지 테스트")
+    void discount() {
+        Price price = new Price(100_000);
+        assertThat(price.discount(Payment.CASH)).isEqualTo(95_000);
+    }
+}
